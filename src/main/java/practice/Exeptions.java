ace932eb5f49541eb58f43a02b3074d899a4a39e
RomaSich <si4poman@gmail.com>
2024-08-30 15:19:32 +0300

    7

---
+++ src/main/java/practice/Exeptions.java
@@ -0,0 +1,44 @@
+package practice;
+
+public class Exeptions {
+    public static void main(String[] args) {
+//method1();
+        uncheckbaleExeption(2, 0);
+        System.out.println("=================");
+        try {
+            first();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static void first() throws InterruptedException {
+        second();
+    }
+    public static void second() throws InterruptedException {
+        Thread.sleep(1000);
+    }
+
+    public static void method1(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.fillInStackTrace();
+        }finally {
+            System.out.println("==================");
+        }
+    }
+    public static void uncheckbaleExeption(int a, int b)
+    {
+        int res;
+        try {
+            res = a/b;
+        }
+        catch (ArithmeticException)
+        {
+            res = 0;
+            System.out.println("create exception");
+        }
+        System.out.println(res);
+    }
+}
